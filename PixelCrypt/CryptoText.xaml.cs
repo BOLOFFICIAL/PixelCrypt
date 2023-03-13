88e63e0888ed54f0cd46b14f4ebb131af0ce7c2d
Bolonin  Official <boloninnn@gmail.com>
2023-03-13 18:29:02 +0300

    Завершена часть с Текстом

--- PixelCrypt/CryptoText.xaml.cs
+++ PixelCrypt/CryptoText.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PixelCrypt
 {
@@ -20,16 +14,21 @@ namespace PixelCrypt
     /// </summary>
     public partial class CryptoText : Page
     {
+        private Button selected_button = new Button();
+        private string image_path = "";
+        private string file_path = "";
+
         public CryptoText()
         {
             InitializeComponent();
             Initialization();
         }
 
-        private void Initialization() 
+        private void Initialization()
         {
             Button_Encrypt.Background = new SolidColorBrush(Colors.White);
             Button_Encrypt.BorderBrush = new SolidColorBrush(Colors.White);
+            selected_button = Button_Encrypt;
         }
 
         private void Button_Menu_Click(object sender, RoutedEventArgs e)
@@ -39,22 +38,150 @@ namespace PixelCrypt
 
         private void Button_Encrypt_Click(object sender, RoutedEventArgs e)
         {
-
+            Button_Encrypt.Background = new SolidColorBrush(Colors.White);
+            Button_Encrypt.BorderBrush = new SolidColorBrush(Colors.White);
+            Button_Decrypt.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            Button_Decrypt.BorderBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            selected_button = Button_Encrypt;
+            Button_Crypto.Content = selected_button.Content;
+            TextBox_Name.IsReadOnly = false;
+            if (!(file_path.Length > 0))
+            {
+                TextBox_Text.IsReadOnly = false;
+                Button_File.Content = "Выбрать фаил для чтения";
+            }
+            else
+            {
+                TextBox_Text.IsReadOnly = true;
+                TextBox_Text.Text = File.ReadAllText(file_path);
+            }
+            if (!(file_path.Length > 0))
+            {
+                Button_File.Content = "Выбрать фаил для чтения";
+            }
+            else
+            {
+                Button_File.Content = "Фаил для чтения " + Path.GetFileName(file_path);
+            }
         }
 
         private void Button_Decrypt_Click(object sender, RoutedEventArgs e)
         {
-
+            TextBox_Name.Text = "";
+            if (image_path.Length > 0)
+            {
+                TextBox_Name.Text = Path.GetFileName(image_path);
+            }
+            else 
+            {
+                Button_File.Content = "Выбрать фаил для записи";
+            }
+            if (!(file_path.Length > 0))
+            {
+                Button_File.Content = "Выбрать фаил для записи";
+            }
+            else
+            {
+                Button_File.Content = "Фаил для записи " + Path.GetFileName(file_path);
+            }
+            Button_Decrypt.Background = new SolidColorBrush(Colors.White);
+            Button_Decrypt.BorderBrush = new SolidColorBrush(Colors.White);
+            Button_Encrypt.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            Button_Encrypt.BorderBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+            selected_button = Button_Decrypt;
+            TextBox_Name.IsReadOnly = true;
+            Button_Crypto.Content = selected_button.Content;
+            TextBox_Text.IsReadOnly = true;
+            TextBox_Text.Text = "";
         }
 
         private void Button_Image_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image(*.jpg;*.png;*.jpeg;)|*.jpg;*.png;*.jpeg;";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                image_path = openFileDialog.FileName;
+                Image_Image.Source = new BitmapImage(new Uri(image_path));
+                TextBox_Name.Text = Path.GetFileNameWithoutExtension(image_path)+"_PixelCrypt";
+            }
         }
 
         private void Button_Crypto_Click(object sender, RoutedEventArgs e)
         {
+            if (CheckCrypto())
+            {
+                if (selected_button.Content == Button_Encrypt.Content)
+                {
+                    TextCrypto.EncryptPhoto(TextBox_Text.Text, image_path, TextBox_Key.Text, TextBox_Name.Text);
+                }
+                if (selected_button.Content == Button_Decrypt.Content)
+                {
+                    TextBox_Text.Text = TextCrypto.DecryptPhoto(image_path, TextBox_Key.Text).Result;
+                    if (file_path.Length > 0)
+                    {
+                        File.WriteAllText(file_path, TextBox_Text.Text);
+                    }
+                }
+            }
+        }
 
+        private bool CheckCrypto()
+        {
+            if (Image_Image.Source == null)
+            {
+                MessageBox.Show("Выберите картику");
+                return false;
+            }
+            if (TextBox_Name.Text.Length < 1)
+            {
+                MessageBox.Show("Ввндите имя картинки");
+                return false;
+            }
+            if (TextBox_Key.Text.Length < 1)
+            {
+                MessageBox.Show("Введите ключ");
+                return false;
+            }
+            return true;
+        }
+
+        private void Button_File_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                file_path = openFileDialog.FileName;
+                if (selected_button.Content == Button_Encrypt.Content)
+                {
+                    Button_File.Content = "Фаил для чтения " + Path.GetFileName(file_path);
+                }
+                else
+                {
+                    Button_File.Content = "Фаил для записи " + Path.GetFileName(file_path);
+                }
+                
+                if (selected_button.Content == Button_Encrypt.Content)
+                {
+                    TextBox_Text.Text = File.ReadAllText(file_path);
+                }
+                TextBox_Text.IsReadOnly = true;
+            }
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            file_path = "";
+            if (selected_button.Content == Button_Encrypt.Content)
+            {
+                Button_File.Content = "Выбрать фаил для чтения";
+            }
+            else 
+            { 
+                Button_File.Content = "Выбрать фаил для записи"; 
+            }
+            TextBox_Text.IsReadOnly = false;
         }
     }
 }
---
+++ PixelCrypt/TextCrypto.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace PixelCrypt
+{
+    public class TextCrypto
+    {
+        public static async void EncryptPhoto(string message, string imagePath, string key,string name)
+        {
+            int binaryIndex = 0;
+            var binaryMessage = TextToBinary(message);
+            binaryMessage = EncryptBinaryText(binaryMessage, key);
+            binaryMessage += "0000000000000000";
+            using (var image = Image.FromFile(imagePath))
+            using (var bitmap = new Bitmap(image))
+            {
+                Color pixel = new Color();
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        pixel = bitmap.GetPixel(i, j);
+                        bitmap.SetPixel(i, j, EncryptPixel(pixel, binaryMessage[binaryIndex]));
+                        binaryIndex++;
+                        if (binaryIndex == binaryMessage.Length)
+                        {
+                            string newpath = RenameFile(imagePath, name);
+                            bitmap.Save(newpath);
+                            MessageBox.Show($"Зашифрованная картинка\n{newpath}\nСохранена");
+                            return;
+                        }
+                    }
+                }
+            }
+            await Task.Delay(1);
+        }
+
+        private static string EncryptBinaryText(string binaryText, string key)
+        {
+            byte[] bytes = SplitBinaryString(binaryText).Select(BinaryToByte).ToArray();
+            StringBuilder result = new StringBuilder(bytes.Length * 8);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                int charCode = bytes[i] ^ (int)key[i % key.Length];
+                result.Append(ByteToBinary((byte)charCode));
+            }
+            return result.ToString();
+        }
+
+        public static async Task<string> DecryptPhoto(string imagePath, string key)
+        {
+            StringBuilder result = new StringBuilder();
+            using (var image = Image.FromFile(imagePath))
+            using (var bitmap = new Bitmap(image))
+            {
+                Color pixel = new Color();
+                for (int i = 0; i < image.Width; i++)
+                {
+                    for (int j = 0; j < image.Height; j++)
+                    {
+                        pixel = bitmap.GetPixel(i, j);
+                        result.Append(DecryptPixel(pixel));
+                        if (result.Length % 16 == 0 && CheckEnd(result))
+                        {
+                            var output = result.ToString();
+                            output = output.Remove(result.Length - 16);
+                            return BinaryToText(EncryptBinaryText(output, key));
+                        }
+                    }
+                }
+            }
+            await Task.Delay(1);
+            return " ";
+        }
+
+
+
+        private static string TextToBinary(string text)
+        {
+            return string.Concat(text.Select(c => Convert.ToString(c, 2).PadLeft(16, '0')));
+        }
+
+        private static string ByteToBinary(byte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+
+        private static byte BinaryToByte(string binary)
+        {
+            return Convert.ToByte(binary, 2);
+        }
+
+        private static string BinaryToText(string binary)
+        {
+            return string.Concat(Enumerable.Range(0, binary.Length / 16)
+                .Select(i => (char)Convert.ToInt32(binary.Substring(i * 16, 16), 2)));
+        }
+
+
+        private static Color EncryptPixel(Color pixel, char binaryValue)
+        {
+            int red = pixel.R;
+            int green = pixel.G;
+            if (red == 255)
+            {
+                red--;
+            }
+            if (green == 255)
+            {
+                green--;
+            }
+            if ((red + green) % 2 == 0)
+            {
+                green++;
+            }
+            green -= int.Parse(binaryValue.ToString());
+            return Color.FromArgb(red, green, pixel.B);
+        }
+
+        private static string DecryptPixel(Color colorPixel)
+        {
+            return ((colorPixel.R + colorPixel.G) % 2 == 0) ? ("1") : ("0");
+        }
+
+
+        private static bool CheckEnd(StringBuilder stringBuilder)
+        {
+            if (stringBuilder.Length >= 16 && stringBuilder.Length % 16 == 0)
+            {
+                return stringBuilder.ToString().Substring(stringBuilder.ToString().Length - 16).Equals("0000000000000000");
+            }
+            return false;
+        }
+
+        private static string[] SplitBinaryString(string binaryString)
+        {
+            int binaryStringLength = binaryString.Length;
+            int arrayLength = (binaryStringLength + 7) / 8;
+            string[] result = new string[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                int startIndex = i * 8;
+                int substringLength = Math.Min(8, binaryStringLength - startIndex);
+                result[i] = binaryString.Substring(startIndex, substringLength).PadLeft(8, '0');
+            }
+            return result;
+        }
+
+        public static string RenameFile(string filePath,string name)
+        {
+            string directory = Path.GetDirectoryName(filePath);
+            string extension = Path.GetExtension(filePath);
+            string newFilePath = Path.Combine(directory, name + extension);
+            return newFilePath;
+        }
+    }
+}
