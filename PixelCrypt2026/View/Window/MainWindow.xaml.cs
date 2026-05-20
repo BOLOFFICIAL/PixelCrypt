f4f87978c65f70885a525a8c3ee457258b0b841c
Bolofficial <boloninnn@gmail.com>
2026-05-20 19:16:28 +0300

    Базовый набор для версии 2026

---
+++ PixelCrypt2026/View/Window/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PixelCrypt2026.ViewModel.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PixelCrypt2026.View.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
---
+++ PixelCrypt2026/ViewModel/Window/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PixelCrypt2026.View.Page;
+using PixelCrypt2026.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelCrypt2026.ViewModel.Window
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private System.Windows.Controls.Page _currentPage = new MainPage();
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public System.Windows.Controls.Page CurrentPage
+        {
+            get => _currentPage;
+            set => Set(ref _currentPage, value);
+        }
+    }
+}
