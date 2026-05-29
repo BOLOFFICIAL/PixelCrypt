1e4b1edf19b9d2270aa300489aecd9d21f0f7126
Bolofficial <boloninnn@gmail.com>
2026-05-30 00:01:15 +0300

    Вынесение контекстного меню в список объеектов

--- PixelCrypt2026/ViewModel/UserControl/ImageChipViewModel.cs
+++ PixelCrypt2026/ViewModel/UserControl/ImageChipViewModel.cs
@@ -7,71 +7,14 @@ namespace PixelCrypt2026.ViewModel.UserControl
 {
     class ImageChipViewModel
     {
-        private readonly ImageListViewModel _parent;
-
         public string ImagePath { get; }
 
         public string FileName { get; }
 
-        public ICommand MoveUpCommand { get; }
-
-        public ICommand MoveDownCommand { get; }
-
-        public ICommand RemoveCommand { get; }
-
-        public ICommand OpenOriginalCommand { get; }
-
-        public ImageChipViewModel(string filePath, ImageListViewModel parent)
+        public ImageChipViewModel(string filePath)
         {
-            _parent = parent;
-
             FileName = Path.GetFileName(filePath);
             ImagePath = filePath;
-
-            MoveUpCommand = new LambdaCommand(OnMoveUp, CanMoveUp);
-            MoveDownCommand = new LambdaCommand(OnMoveDown, CanMoveDown);
-            RemoveCommand = new LambdaCommand(OnRemove);
-            OpenOriginalCommand = new LambdaCommand(OnOpenOriginal);
-        }
-
-        private void OnMoveUp(object p)
-        {
-            int index = _parent.Images.IndexOf(this);
-
-            if (index <= 0)
-                return;
-
-            _parent.Images.Move(index, index - 1);
-        }
-
-        private bool CanMoveUp(object p)
-            => _parent.Images.IndexOf(this) > 0;
-
-        private void OnMoveDown(object p)
-        {
-            int index = _parent.Images.IndexOf(this);
-
-            if (index < 0 || index >= _parent.Images.Count - 1)
-                return;
-
-            _parent.Images.Move(index, index + 1);
-        }
-
-        private bool CanMoveDown(object p)
-            => _parent.Images.IndexOf(this) < _parent.Images.Count - 1;
-
-        private void OnRemove(object p)
-        {
-            _parent.Images.Remove(this);
-        }
-
-        private void OnOpenOriginal(object p)
-        {
-            Process.Start(new ProcessStartInfo()
-            {
-                FileName = ImagePath,
-                UseShellExecute = true
-            });
         }
     }
 }
--- PixelCrypt2026/ViewModel/UserControl/ImageListViewModel.cs
+++ PixelCrypt2026/ViewModel/UserControl/ImageListViewModel.cs
@@ -2,6 +2,7 @@
 using PixelCrypt2026.Commands.Base;
 using PixelCrypt2026.ViewModel.Base;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace PixelCrypt2026.ViewModel.UserControl
@@ -16,12 +17,25 @@ namespace PixelCrypt2026.ViewModel.UserControl
 
         public ICommand ClearImagesCommand { get; set; }
 
+        public ICommand MoveUpCommand { get; }
+
+        public ICommand MoveDownCommand { get; }
+
+        public ICommand RemoveCommand { get; }
+
+        public ICommand OpenOriginalCommand { get; }
+
         public ImageListViewModel()
         {
             Images = new ObservableCollection<ImageChipViewModel>();
 
             AddImageCommand = new LambdaCommand(AddImage);
             ClearImagesCommand = new LambdaCommand(ClearImages);
+
+            MoveUpCommand = new LambdaCommand(OnMoveUp, CanMoveUp);
+            MoveDownCommand = new LambdaCommand(OnMoveDown, CanMoveDown);
+            RemoveCommand = new LambdaCommand(OnRemove);
+            OpenOriginalCommand = new LambdaCommand(OnOpenOriginal);
         }
 
         public ImageChipViewModel SelectedImage
@@ -52,13 +66,72 @@ namespace PixelCrypt2026.ViewModel.UserControl
                 if (alreadyExists)
                     continue;
 
-                Images.Add(new ImageChipViewModel(filePath, this));
+                Images.Add(new ImageChipViewModel(filePath));
             }
         }
 
         private void ClearImages(object p)
         {
             Images.Clear();
+            SelectedImage = null;
+        }
+
+        private void OnMoveUp(object p)
+        {
+            if (p is not ImageChipViewModel image) return;
+
+            int index = Images.IndexOf(image);
+
+            if (index <= 0)
+                return;
+
+            Images.Move(index, index - 1);
+        }
+
+        private bool CanMoveUp(object p) 
+        {
+            if (p is not ImageChipViewModel image)
+                return false;
+
+            return Images.IndexOf(image) > 0;
+        }
+
+        private void OnMoveDown(object p)
+        {
+            if (p is not ImageChipViewModel image) return;
+
+            int index = Images.IndexOf(image);
+
+            if (index < 0 || index >= Images.Count - 1)
+                return;
+
+            Images.Move(index, index + 1);
+        }
+
+        private bool CanMoveDown(object p) 
+        {
+            if (p is not ImageChipViewModel image)
+                return false;
+
+            return Images.IndexOf(image) < Images.Count - 1;
+        }
+
+        private void OnRemove(object p)
+        {
+            if (p is not ImageChipViewModel image) return;
+
+            Images.Remove(image);
+        }
+
+        private void OnOpenOriginal(object p)
+        {
+            if (p is not ImageChipViewModel image) return;
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = image.ImagePath,
+                UseShellExecute = true
+            });
         }
     }
 }
