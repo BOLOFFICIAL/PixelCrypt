b12ef98b97fa709bd9c42b91b48a3e19ad499f6a
Bolofficial <boloninnn@gmail.com>
2026-05-31 17:24:33 +0300

    Добавление BindablePasswordBox

---
+++ PixelCrypt2026/View/UserControl/BindablePasswordBox.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixelCrypt2026.View.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : System.Windows.Controls.UserControl
+    {
+        private bool _isPasswordChanged;
+
+        public string Password
+        {
+            get => (string)GetValue(PasswordProperty);
+            set => SetValue(PasswordProperty, value);
+        }
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox),
+                new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, PasswordPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+
+        private static void PasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is BindablePasswordBox passwordBox)
+            {
+                passwordBox.UpdatePassword();
+            }
+        }
+
+        private void UpdatePassword()
+        {
+            if (_isPasswordChanged) return;
+
+            passwordBox.Password = Password;
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            _isPasswordChanged = true;
+            Password = passwordBox.Password;
+            _isPasswordChanged = false;
+        }
+    }
+}
