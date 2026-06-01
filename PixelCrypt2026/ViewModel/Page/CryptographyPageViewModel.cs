2e2ab88ee6a8a2c404111f2596eec722f2038576
Болонин Михаил <bolonin.m@bit76.ru>
2026-06-01 09:31:25 +0300

    Добавлен контрол для отображения прогресса, черновая логика отмены выполнения текущей операции

--- PixelCrypt2026/ViewModel/Page/CryptographyPageViewModel.cs
+++ PixelCrypt2026/ViewModel/Page/CryptographyPageViewModel.cs
@@ -8,33 +8,114 @@ namespace PixelCrypt2026.ViewModel.Page
 {
     internal class CryptographyPageViewModel : BasePageLayoutViewModel
     {
+        private CancellationTokenSource _cancellationTokenSource;
+        private bool _isProcessing;
+
         public ImageListViewModel ImageList { get; set; }
+        public ProgressPanelViewModel Progress { get; set; }
 
         public ICommand DoCommand { get; set; }
+        public ICommand StopCommand { get; set; }
 
         public CryptographyPageViewModel(NavigationService navigation) : base(navigation)
         {
             Title = $"Шифрование";
             ImageList = new ImageListViewModel();
+            Progress = new ProgressPanelViewModel();
 
             DoCommand = new LambdaCommand(OnDoCommand, CanDoCommand);
+            StopCommand = new LambdaCommand(OnStopCommand, CanStopCommand);
         }
 
         private async void OnDoCommand(object obj)
         {
+            if (_isProcessing)
+                return;
+
+            _isProcessing = true;
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
             ImageList.IsEnable = false;
 
-            foreach (var el in ImageList.Images)
+            try
             {
-                ImageList.SelectedImage = el;
+                int totalItems = ImageList.Images.Count;
+                int processedItems = 0;
 
-                await Task.Delay(1000);
+                Progress.ProgressValue = 0;
+                Progress.ProgressTime = "Вычисление...";
+
+                DateTime startTime = DateTime.Now;
+
+                foreach (var el in ImageList.Images)
+                {
+                    // Проверка на отмену
+                    token.ThrowIfCancellationRequested();
+
+                    ImageList.SelectedImage = el;
+
+                    try
+                    {
+                        await Task.Delay(1000, token);
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        break;
+                    }
+
+                    processedItems++;
+
+                    double progressPercent = (double)processedItems / totalItems * 100;
+                    Progress.ProgressValue = progressPercent;
+
+                    TimeSpan elapsed = DateTime.Now - startTime;
+                    double avgTimePerItem = elapsed.TotalSeconds / processedItems;
+                    int remainingItems = totalItems - processedItems;
+                    double estimatedRemainingSeconds = avgTimePerItem * remainingItems;
+
+                    TimeSpan remaining = TimeSpan.FromSeconds(estimatedRemainingSeconds);
+                    Progress.ProgressTime = $"{(int)remaining.TotalHours:D2}:{remaining.Minutes:D2}:{remaining.Seconds:D2}";
+                }
+
+                if (token.IsCancellationRequested)
+                {
+                    Progress.ProgressTime = $"Остановлено ({processedItems}/{totalItems})";
+                }
+                else
+                {
+                    Progress.ProgressValue = 100;
+                    Progress.ProgressTime = "Завершено";
+                }
             }
-
-            ImageList.IsEnable = true;
+            catch (OperationCanceledException)
+            {
+                Progress.ProgressTime = "Операция отменена";
+            }
+            catch (Exception ex)
+            {
+                Progress.ProgressTime = "Ошибка";
+                System.Diagnostics.Debug.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                _isProcessing = false;
+                ImageList.IsEnable = true;
+                _cancellationTokenSource?.Dispose();
+                _cancellationTokenSource = null;
+            }
         }
 
         private bool CanDoCommand(object obj)
             => ImageList.Images.Count > 0;
+
+        private void OnStopCommand(object obj)
+        {
+            _cancellationTokenSource?.Cancel();
+            Progress.ProgressTime = "Остановка...";
+        }
+
+        private bool CanStopCommand(object obj)
+            => _isProcessing && _cancellationTokenSource != null;
     }
 }
---
+++ PixelCrypt2026/ViewModel/UserControl/ProgressPanelViewModel.cs
@@ -0,0 +1,46 @@
+﻿using PixelCrypt2026.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelCrypt2026.ViewModel.UserControl
+{
+    class ProgressPanelViewModel : BaseViewModel
+    {
+        private string _progressTime = "00:00:00";
+        private string _progressPercent = "0%";
+        private double _progressValue;
+
+        public string ProgressTime
+        {
+            get => _progressTime;
+            set => Set(ref _progressTime, value);
+        }
+
+        public string ProgressPercent
+        {
+            get => _progressPercent;
+            private set => Set(ref _progressPercent, value);
+        }
+
+        public double ProgressValue
+        {
+            get => _progressValue;
+            set
+            {
+                if (Set(ref _progressValue, value))
+                {
+                    ProgressPercent = $"{value:F1}%";
+                }
+            }
+        }
+
+        public ProgressPanelViewModel()
+        {
+            ProgressValue = 0;
+            ProgressTime = "--:--:--";
+        }
+    }
+}
