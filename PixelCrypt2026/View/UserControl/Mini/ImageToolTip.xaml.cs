8287c5ca1921905a82ce40bbc662721177c01ca8
Болонин Михаил <bolonin.m@bit76.ru>
2026-07-16 16:31:50 +0300

    Вынесение ToolTip в отдельные usercontrol

---
+++ PixelCrypt2026/View/UserControl/Mini/ImageToolTip.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+
+namespace PixelCrypt2026.View.UserControl.Mini
+{
+    /// <summary>
+    /// Логика взаимодействия для ImageToolTip.xaml
+    /// </summary>
+    public partial class ImageToolTip : System.Windows.Controls.UserControl
+    {
+        public ImageToolTip()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName
+        {
+            get => (string)GetValue(FileNameProperty);
+            set => SetValue(FileNameProperty, value);
+        }
+
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register(nameof(FileName), typeof(string), typeof(ImageToolTip));
+
+        public string FileSizeFormatted
+        {
+            get => (string)GetValue(FileSizeFormattedProperty);
+            set => SetValue(FileSizeFormattedProperty, value);
+        }
+
+        public static readonly DependencyProperty FileSizeFormattedProperty =
+            DependencyProperty.Register(nameof(FileSizeFormatted), typeof(string), typeof(ImageToolTip));
+
+        public string ImageResolution
+        {
+            get => (string)GetValue(ImageResolutionProperty);
+            set => SetValue(ImageResolutionProperty, value);
+        }
+
+        public static readonly DependencyProperty ImageResolutionProperty =
+            DependencyProperty.Register(nameof(ImageResolution), typeof(string), typeof(ImageToolTip));
+
+        public string FilePath
+        {
+            get => (string)GetValue(FilePathProperty);
+            set => SetValue(FilePathProperty, value);
+        }
+
+        public static readonly DependencyProperty FilePathProperty =
+            DependencyProperty.Register(nameof(FilePath), typeof(string), typeof(ImageToolTip));
+
+        public string Foreground
+        {
+            get => (string)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty ForegroundProperty =
+            DependencyProperty.Register(nameof(Foreground), typeof(string), typeof(ImageToolTip));
+    }
+}
---
+++ PixelCrypt2026/View/UserControl/Mini/InfoToolTip.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace PixelCrypt2026.View.UserControl.Mini
+{
+    /// <summary>
+    /// Логика взаимодействия для ImageToolTip.xaml
+    /// </summary>
+    public partial class InfoToolTip : System.Windows.Controls.UserControl
+    {
+        public InfoToolTip()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(InfoToolTip));
+
+        public string Info
+        {
+            get => (string)GetValue(InfoProperty);
+            set => SetValue(InfoProperty, value);
+        }
+
+        public static readonly DependencyProperty InfoProperty =
+            DependencyProperty.Register(nameof(Info), typeof(string), typeof(InfoToolTip));
+    }
+}
