08b243784d593cd90c15e08c5cf72d7e98231fa1
Bolofficial <boloninnn@gmail.com>
2026-07-30 23:45:02 +0300

    Корректировки UIUX

--- PixelCrypt2026/ViewModel/UserControl/PasswordBoxViewModel.cs
+++ PixelCrypt2026/ViewModel/UserControl/PasswordBoxViewModel.cs
@@ -7,24 +7,36 @@ namespace PixelCrypt2026.ViewModel.UserControl
 {
     internal class PasswordBoxViewModel : BaseViewModel
     {
+        private const int _passwordLength = 8;
+        private const string _passwordAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+
         private string _password;
         private GridLength _hideWidth;
         private GridLength _visibleWidth;
+        private GridLength _autoPasswordWidth;
         private bool isVisible = true;
         private string _toggleVisibilityIcon;
 
         public ICommand ToggleVisibilityCommand { get; }
+        public ICommand AutoPasswordCommand { get; }
 
         public PasswordBoxViewModel()
         {
             ToggleVisibilityCommand = new LambdaCommand(OnToggleVisibilityCommand);
+            AutoPasswordCommand = new LambdaCommand(OnAutoPasswordCommand);
             OnToggleVisibilityCommand(null);
         }
 
         public string Password
         {
             get => _password;
-            set => Set(ref _password, value);
+            set
+            {
+                Set(ref _password, value);
+                AutoPasswordWidth = string.IsNullOrEmpty(_password)
+                    ? new GridLength(1, GridUnitType.Auto)
+                    : new GridLength(0, GridUnitType.Pixel);
+            }
         }
 
         public GridLength HideWidth
@@ -37,6 +49,12 @@ namespace PixelCrypt2026.ViewModel.UserControl
         {
             get => _visibleWidth;
             set => Set(ref _visibleWidth, value);
+        }
+
+        public GridLength AutoPasswordWidth
+        {
+            get => _autoPasswordWidth;
+            set => Set(ref _autoPasswordWidth, value);
         }
 
         public string ToggleVisibilityIcon
@@ -61,6 +79,14 @@ namespace PixelCrypt2026.ViewModel.UserControl
                 VisibleWidth = new GridLength(0, GridUnitType.Star);
                 ToggleVisibilityIcon = "Regular_EyeSlash";
             }
+        }
+
+        private void OnAutoPasswordCommand(object obj)
+        {
+            var random = new Random();
+
+            Password = new string(Enumerable.Repeat(_passwordAlphabet, _passwordLength)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
