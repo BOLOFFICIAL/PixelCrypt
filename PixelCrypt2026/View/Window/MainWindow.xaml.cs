eedd3c4eefd79e333bcfd00ec2ddece7c1c3144e
Болонин Михаил <bolonin.m@bit76.ru>
2026-08-17 11:37:50 +0300

    Кастомная шапка окна

--- PixelCrypt2026/View/Window/MainWindow.xaml.cs
+++ PixelCrypt2026/View/Window/MainWindow.xaml.cs
@@ -4,6 +4,10 @@ using PixelCrypt2026.Program.Service;
 using PixelCrypt2026.View.Page;
 using PixelCrypt2026.ViewModel.Page;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace PixelCrypt2026.View.Window
 {
@@ -14,11 +18,17 @@ namespace PixelCrypt2026.View.Window
     {
         public static MainWindow Instance { get; private set; }
 
+        private const int WM_GETMINMAXINFO = 0x0024;
+        private const int MONITOR_DEFAULTTONEAREST = 0x00000002;
+
         public MainWindow()
         {
             InitializeComponent();
 
             Instance = this;
+
+            SourceInitialized += OnSourceInitialized;
+            StateChanged += (s, e) => UpdateMaximizeButton();
 
             ProgramHelper.CleanupTempFiles();
 
@@ -29,6 +39,70 @@ namespace PixelCrypt2026.View.Window
             navigation.Register<SteganographyPageViewModel>(new SteganographyPage(navigation));
 
             navigation.NavigateTo<MainPageViewModel>();
+        }
+
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd)?.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_GETMINMAXINFO)
+            {
+                WmGetMinMaxInfo(hwnd, lParam);
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            var mmi = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+
+            var monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (monitor != IntPtr.Zero)
+            {
+                var monitorInfo = new MONITORINFO { cbSize = Marshal.SizeOf<MONITORINFO>() };
+                if (GetMonitorInfo(monitor, ref monitorInfo))
+                {
+                    var work = monitorInfo.rcWork;
+                    var area = monitorInfo.rcMonitor;
+                    mmi.ptMaxPosition.x = work.left - area.left;
+                    mmi.ptMaxPosition.y = work.top - area.top;
+                    mmi.ptMaxSize.x = work.right - work.left;
+                    mmi.ptMaxSize.y = work.bottom - work.top;
+                }
+            }
+
+            var dpi = VisualTreeHelper.GetDpi(this);
+            mmi.ptMinTrackSize.x = (int)(MinWidth * dpi.PixelsPerInchX / 96.0);
+            mmi.ptMinTrackSize.y = (int)(MinHeight * dpi.PixelsPerInchY / 96.0);
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void UpdateMaximizeButton()
+        {
+            var maximized = WindowState == WindowState.Maximized;
+            MaximizeButton.Visibility = maximized ? Visibility.Collapsed : Visibility.Visible;
+            RestoreButton.Visibility = maximized ? Visibility.Visible : Visibility.Collapsed;
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -44,5 +118,46 @@ namespace PixelCrypt2026.View.Window
 
             base.OnClosing(e);
         }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MONITORINFO
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public int dwFlags;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromWindow(IntPtr hwnd, int dwFlags);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
     }
 }
